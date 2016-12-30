bcd60b5f7672ae557028bdfed4e84215a70a9a6d
Rafael Espinoza <nerddiffer@gmail.com>
2016-12-29 21:44:56 -0800

    feat(index): Read & parse companies from .md file
    Ideas for detecting a markdown table are straight from the 'markdown-it'
    source code for table rules:
    https://github.com/markdown-it/markdown-it/blob/master/lib/rules_block/table.js

--- index.js
+++ index.js
@@ -14,3 +14,64 @@ readFile(PATHS.DATA_DEFAULT, data => {
   console.log(parse.parseCompany(company));
   console.log(parse.parseLocation(locations));
 });
+
+const parseCompanies = () => {
+  readFile(PATHS.DATA_DEFAULT, data => {
+    const arr = data.split("\n");
+    const len = arr.length;
+
+    let hasTableStarted = false;
+    let i = 0;
+
+    // find the start of the table
+    do {
+      if (!hasTableStarted && detectTable(arr, i)) {
+        hasTableStarted = true;
+        break;
+      }
+
+      i += 1;
+    } while (i < len);
+
+    i += 2; // jump to first table row
+
+    let line = arr[i];
+    const { parseLine, parseCompany, parseLocation } = parse;
+
+    // just read, assuming they're all table rows
+    while (i < len) {
+      if (line === '') { break; } // assumes last line or end of table is an empty string
+
+      const { company, locations } = parseLine(line);
+      const parsedLine = {
+        company: parseCompany(company),
+        locations: parseLocation(locations)
+      };
+
+      console.log(parsedLine);
+
+      line = arr[i += 1];
+    }
+  });
+};
+
+function detectTable(arr, ind) {
+  const len = arr.length;
+
+  // needs 3 or more lines
+  if (len - ind < 3) { return false; }
+
+  // first character of the second line should be '|', '-', ':',
+  // and no other characters are allowed but spaces;
+  // basically, this is the equivalent of /^[-:|][-:|\s]*$/ regexp
+
+  if (!(/^[-:|][-:|\s]*$/.test(arr[ind + 1]))) { return false; }
+
+  // TODO:
+  // count number of columns in current, next & row after that.
+  // if all the same, then you're in a table.
+
+  return true; // for now, this is fine...
+}
+
+parseCompanies();

54a4e28d2bd5d9514c0e817a2094f6e414474075
Rafael Espinoza <nerddiffer@gmail.com>
2016-12-29 17:37:20 -0800

    feat(parse): Add more methods to parse module
    feat(parse): Add #parseLine helper
    
    feat(parse): Add #parseCompany method
    
    fix(parse): Patch up edge cases for #parseLocation
    
    fix(parse): Handle no input

--- src/parse.js
+++ src/parse.js
@@ -1,22 +1,80 @@
+const preProcessLine = str => (
+  str.split('|')
+    .map(s => s.trim())
+    .filter(s => s.length > 0)
+);
+
+module.exports.parseLine = str => {
+  const processedLine = preProcessLine(str);
+
+  return {
+    company: processedLine[0],
+    locations: processedLine[1]
+  };
+};
+
+module.exports.parseCompany = (str = '') => {
+  const len = str.length;
+
+  if (len === 0) { return; }
+
+  const output = { name: null, link: null };
+
+  let i = 1;
+  let name = '';
+
+  // capture company name
+  while (i < len) {
+    name += str[i];
+
+    if (/\]/.test(str[i + 1])) {
+      output.name = name;
+      break;
+    }
+
+    i += 1;
+  }
+
+  // number of spaces between last char in name & first char in link
+  const DIST = 3;
+  output.link = str.slice(i + DIST, len - 1);
+
+  return output;
+};
+
+const isUK = str => str === 'UK';
+const isDC = (str1, str2) => str1 === 'Washington' && str2 === 'D.C.';
 // does the string "look like" an abbreviation for a state?
-const isStatey = str => str.length === 2 && /[A-Z]/.test(str);
+const isStatey = str => str.length === 2 && !isUK(str) && /[A-Z]/.test(str);
 const isRemote = str => str && /^remote$/i.test(str);
 
-module.exports.parseLocation = str => {
+module.exports.parseLocation = (str = '') => {
   const inputArr = str.split(';');
 
   return inputArr.reduce((output, candidate) => {
     const parts = candidate.split(',').map(part => part.trim());
 
     let obj;
+    const len = parts.length;
 
-    if (isRemote(parts[0])) {
-      obj = { remote: true };
-    } else if (parts.length === 2 && !isStatey(parts[1])) {
-      obj = {
-        city: parts[0],
-        country: parts[1]
-      };
+    if (len === 1) {
+      if (isRemote(parts[0])) {
+        obj = { remote: true };
+      } else {
+        obj = { country: parts[0] };
+      }
+    } else if (len === 2 && !isStatey(parts[1])) {
+      if (isDC(parts[0], parts[1])) {
+        obj = {
+          city: parts[0] + ', ' + parts[1],
+          country: 'USA'
+        };
+      } else {
+        obj = {
+          city: parts[0],
+          country: parts[1]
+        };
+      }
     } else {
       obj = {
         city: parts[0],
